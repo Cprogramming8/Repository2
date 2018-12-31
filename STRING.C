ccecf9335aef80c8fb88d82dc4c213b96625fdc5
sowmya95simma95lalith <46022985+sowmya95simma95lalith@users.noreply.github.com>
2018-12-31 09:45:03 +0530

    Add files via upload

---
+++ STRING.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char s[100]="welcome to c programming.";
+clrscr();
+printf("hello world");
+printf("\n%s",s);
+getch();
+}
