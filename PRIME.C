14c8c0bbb857304650091fec2aae211d0472cc62
sowmya95simma95lalith <46022985+sowmya95simma95lalith@users.noreply.github.com>
2019-01-04 06:46:17 +0530

    Add files via upload

---
+++ PRIME.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,n,count=0;
+clrscr();
+printf("enter n value");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+ count=0;
+ for(j=1;j<=i;j++)
+ {
+
+  if(i%j==0)
+  {
+	count++;
+  }
+
+ }
+  if(count==2)
+  {
+	printf("\n %d",i);
+  }
+}
+getch();
+}
