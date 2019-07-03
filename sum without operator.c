752c42ecaaeae96ce6ac3eacd905027f82fb7195
sowmya95simma95lalith <46022985+sowmya95simma95lalith@users.noreply.github.com>
2019-07-03 21:07:41 +0530

    Add files via upload

---
+++ sum without operator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,sum;
+    printf("enter a,b values");
+    scanf("%d%d",&a,&b);
+    printf("sum=%d",sum=a-(-b));
+    return 0;
+}
